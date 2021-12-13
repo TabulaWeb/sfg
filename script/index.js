793cc4d811919895e837a1f3688e8d7b2ce49df8
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-12-13 22:24:33 +0300

    991px

--- script/index.js
+++ script/index.js
@@ -144,5 +144,27 @@ window.onscroll = () => {
     document.querySelector('.services-content__img img').style.right = `${pos/9}px`
 }
 
+// О компании выпадающий список
+document.querySelector('.modal-menu__button').addEventListener('click', function(){
+    if(document.querySelector('.modal-menu_list').classList.contains('active')){
+        document.querySelector('.modal-menu_list').classList.remove('active')
+        document.querySelector('.modal-menu__button img').style.transform = 'rotate(180deg)'
+    } else {
+        document.querySelector('.modal-menu_list').classList.add('active')
+        document.querySelector('.modal-menu__button img').style.transform = 'rotate(360deg)'
+    }
+})
+
+// бургер меню
+document.querySelector('.burger-menu').addEventListener('click', function(){
+    if(document.querySelector('.modal-menu').classList.contains('active')){
+        document.querySelector('.modal-menu').classList.remove('active')
+        document.querySelector('.burger-menu').classList.remove('active')
+    } else {
+        document.querySelector('.modal-menu').classList.add('active')
+        document.querySelector('.burger-menu').classList.add('active')
+    }
+})
+
 // Инициализируем анимации
 AOS.init();
