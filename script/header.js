06f3a1da96c3cc34ad7d22ba8c45b83633f1e8a6
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-12-15 03:18:39 +0300

    linebusines

---
+++ script/header.js
@@ -0,0 +1,21 @@
+// бургер меню
+document.querySelector('.burger-menu').addEventListener('click', function(){
+    if(document.querySelector('.modal-menu').classList.contains('active')){
+        document.querySelector('.modal-menu').classList.remove('active')
+        document.querySelector('.burger-menu').classList.remove('active')
+    } else {
+        document.querySelector('.modal-menu').classList.add('active')
+        document.querySelector('.burger-menu').classList.add('active')
+    }
+})
+
+// О компании выпадающий список
+document.querySelector('.modal-menu__button').addEventListener('click', function(){
+    if(document.querySelector('.modal-menu_list').classList.contains('active')){
+        document.querySelector('.modal-menu_list').classList.remove('active')
+        document.querySelector('.modal-menu__button img').style.transform = 'rotate(180deg)'
+    } else {
+        document.querySelector('.modal-menu_list').classList.add('active')
+        document.querySelector('.modal-menu__button img').style.transform = 'rotate(360deg)'
+    }
+})
